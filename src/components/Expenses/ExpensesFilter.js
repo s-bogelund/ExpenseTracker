c1b20a2ba233254b55083f739484ccbeef4210ec
simbj94 <simbj94@gmail.com>
2022-03-02 12:22:19 +0100

    added some mobile styling and added a button to clear the list of expenses that has no logic yet and needs more styling

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import './ExpensesFilter.css';
+import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-  const dropdownChangeHandler = (event) => {
-    props.onChangeFilter(event.target.value);
-  };
+	const dropdownChangeHandler = (event) => {
+		props.onChangeFilter(event.target.value);
+	};
 
-  return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
-        <label>Filtrér efter år</label>
-        <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
-          <option value='2018'>2018</option>
-          <option value='2017'>2017</option>
-        </select>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<div className="expenses-filter">
+				<div className="expenses-filter-left">
+					<button className="delete-list-button">Ryd Liste</button>
+				</div>
+				<div>
+					<div className="expenses-filter__control">
+						<label>Filtrér efter år</label>
+						<select value={props.selected} onChange={dropdownChangeHandler}>
+							<option value="2022">2022</option>
+							<option value="2021">2021</option>
+							<option value="2020">2020</option>
+							<option value="2019">2019</option>
+							<option value="2018">2018</option>
+							<option value="2017">2017</option>
+						</select>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ExpensesFilter;
